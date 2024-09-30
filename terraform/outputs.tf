7d1cbaff4cc324b53b3dce6baaeca7c3482e18a3
stackgen-automation <automation@stackgen.com>
2024-09-30 06:46:15 +0000

    Automated commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,16 @@
 
+output "aws_iam_role_retroboard_role_shaik_15_arn" {
+  value = module.appcd_c3af77cb-66e5-54c7-89fb-8590c5184164.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_retroboard_role_shaik_15_name" {
+  value = module.appcd_c3af77cb-66e5-54c7-89fb-8590c5184164.name
+  sensitive = false
+}
+
+
 output "aws_cloudwatch_log_group_aws_lambda_retroboard_function_shaik_15_arn" {
   value = module.appcd_86526c39-3721-59cc-9339-c435b049175b.arn
   sensitive = false
@@ -23,14 +35,8 @@ output "aws_sqs_reteroboard_15_arn" {
 }
 
 
-output "aws_sns_reterobord_shaik_arn" {
-  value = module.appcd_ea0316db-a29b-5e25-abd1-1c47d4706711.arn
-  sensitive = false
-}
-
-
-output "aws_sns_retero_arn" {
-  value = module.appcd_a9eb943d-15ff-5df4-bda2-d421a7b06d4e.arn
+output "aws_lambda_retroboard_function_shaik_15_invoke_arn" {
+  value = module.appcd_0d0f09f5-5625-5330-94d2-713640e22705.invoke_arn
   sensitive = false
 }
 
@@ -53,19 +59,13 @@ output "aws_lambda_retroboard_function_shaik_15_function_url" {
 }
 
 
-output "aws_lambda_retroboard_function_shaik_15_invoke_arn" {
-  value = module.appcd_0d0f09f5-5625-5330-94d2-713640e22705.invoke_arn
+output "aws_sns_reterobord_shaik_arn" {
+  value = module.appcd_ea0316db-a29b-5e25-abd1-1c47d4706711.arn
   sensitive = false
 }
 
 
-output "aws_iam_role_retroboard_role_arn" {
-  value = module.appcd_c3af77cb-66e5-54c7-89fb-8590c5184164.arn
-  sensitive = false
-}
-
-
-output "aws_iam_role_retroboard_role_name" {
-  value = module.appcd_c3af77cb-66e5-54c7-89fb-8590c5184164.name
+output "aws_sns_retero_arn" {
+  value = module.appcd_a9eb943d-15ff-5df4-bda2-d421a7b06d4e.arn
   sensitive = false
 }
